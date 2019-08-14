b58de8bb8beaae0545bd3c138e33069543014a3b
Ashutosh Sathe <2ashutoshbs@gmail.com>
2019-08-14 20:15:41 +0530

    Print test accuracy at the end of the training

--- train.py
+++ train.py
@@ -9,6 +9,10 @@ SAVE_DIR = './saved_models/resnet18/'
 MODEL_NAME = 'resnet18_at_epoch_{}.pt'
 LOSS_NAME = 'loss_log.csv'
 
+N_TRAIN = 40000
+N_VAL   = 10000
+N_TEST  = 10000
+
 def update_learning_rate(optim, old_lr, new_lr, print_msg=True):
     if print_msg:
         print('\nReducing learning rate from {} to {}'.format(old_lr, new_lr))
@@ -20,7 +24,8 @@ def main():
     if not os.path.exists(SAVE_DIR):
         os.makedirs(SAVE_DIR)
     model = ResNet18().cuda()
-    train_loader, val_loader, test_loader = utils.get_cifar10_data_loaders()
+    train_loader, val_loader, test_loader = utils.get_cifar10_data_loaders(n_train=N_TRAIN, \
+        n_val=N_VAL, n_test=N_TEST)
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-1, momentum=0.9)
     xent = nn.CrossEntropyLoss()
     step = 0
@@ -46,7 +51,16 @@ def main():
             step += 1
             utils.progress(j+1, len(train_loader), 'Batch [{}/{}] Epoch [{}/{}]'.format(j+1,len(train_loader),i+1,n_epochs))
         torch.save(model.state_dict(), MODEL_SAVE_PATH.format(i))
-    print('Done')
+    print('Training Complete')
+    model.eval()
+    correct = 0
+    for j, (images, labels) in enumerate(test_loader):
+        images, labels = images.cuda(), labels.cuda() 
+        logits = model(images)
+        correct += (torch.max(logits, 1)[-1] == labels).sum().item()
+        utils.progress(j+1, len(test_loader), 'Batch [{}/{}]'.format(j+1, len(test_loader)))
+    print('Test Accuracy = {}%'.format(float(correct) * 100.0/N_TEST))
+
 
 if __name__ == '__main__':
     main()
